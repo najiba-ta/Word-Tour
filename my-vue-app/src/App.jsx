9a0a88bf5075fcfeb8b7577f59b4f9cd77e53058
Najiba <irfan@NajibasMacbook.local>
2026-03-26 03:18:49 -0700

    Added Countries Data

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,12 +1,16 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countrys/Countries'
 
+const countriesPromise = fetch ('https://openapi.programming-hero.com/api/all')
+ .then(res => res.json())
 function App() {
- 
-
   return (
     <>
-    <Countries></Countries>      
+      <Suspense fallback={<p>Countries Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+    
     </>
   )
 }
